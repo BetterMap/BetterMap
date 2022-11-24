8f0a754543451352d1fe614da55b323df281aef9
Soopyboo32 <49228220+Soopyboo32@users.noreply.github.com>
2022-11-24 17:09:46 +0800

    babel (#29)

---
+++ build.bat
@@ -0,0 +1,1 @@
+babel --delete-dir-on-start --retain-lines . -d "out/BetterMap" --copy-files --no-copy-ignored --minified --no-comments --ignore "**/babal-substitute-mappings/**","**/.git/**","**/build.bat","**/init.bat","**/node_modules/**"

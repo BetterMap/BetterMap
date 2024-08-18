a6352deaf4ead0461088180171c12e2528b635c3
UnclaimedBloom6 <unclaimedgamer@gmail.com>
2024-08-19 02:37:08 +1200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
 cd "$(dirname "$0")"
+
+rm -rf out/
+
 mkdir out
-cd ..
-zip -r BetterMap/out/BetterMap.zip BetterMap/\
-    -x BetterMap/*.git* BetterMap/build.sh BetterMap/out/
+mkdir out/BetterMap
+
+rsync -av --exclude-from=".gitignore"\
+    --exclude="*.git*"\
+    --exclude="build.sh"\
+    --exclude="README.md"\
+    * out/BetterMap/
+
+cd out/
+zip -r BetterMap.zip BetterMap/
+rm -rf BetterMap
